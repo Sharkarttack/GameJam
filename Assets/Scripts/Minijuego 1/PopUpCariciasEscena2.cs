7c0d3727cc1b6461f72d4034e3f91f2b4ad094c1
0rio1 <oriol.25.ca@gmail.com>
2026-07-16 18:21:46 +0200

    GameJam V.07
    Add graphics to mini game 1

--- Assets/Scripts/Minijuego 1/PopUpCariciasEscena2.cs
+++ Assets/Scripts/Minijuego 1/PopUpCariciasEscena2.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// Objeto interactuable del pop-up de caricias. 
@@ -8,13 +9,43 @@ public class PopUpCariciasEscena2 : InteractableObject
 {
     // Referencia al gestor del minijuego de caricias
     [SerializeField] private CariciasManagerEscena2 cariciasManager;
-
-    /// <summary>
+    [SerializeField] private ParticleSystem corazones;
+    public float goalSpeed;
+    private Vector2 lastMousePos;
+    void Update()
+    {
+        if(Mouse.current.leftButton.IsPressed() && isShaking())
+        {
+            
+            corazones.Play();
+            cariciasManager.hacerMimos();
+        }
+        else
+        {
+            corazones.Stop();
+        }
+    }
+    
+    ///<summary>
     /// Se ejecuta cuando el jugador interactúa con el pop-up
     /// </summary>
     public override void OnInteract()
     {
         // Notifica al gestor que se ha realizado una caricia
-        cariciasManager.hacerMimos();
+        //cariciasManager.hacerMimos();
+    }
+    private bool isShaking()
+    {
+        // Posición actual del ratón.
+        Vector2 currentPos = Mouse.current.position.ReadValue();
+        // Calcula la velocidad del ratón en píxeles por segundo.
+        float speed = (currentPos - lastMousePos).magnitude / Time.deltaTime;
+        lastMousePos = currentPos;
+        // Comprueba si la velocidad supera el umbral.
+        if (speed > goalSpeed)
+        {
+            return true;
+        }
+        return false;
     }
 }
