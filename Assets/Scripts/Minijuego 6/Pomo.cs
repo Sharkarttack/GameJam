a13e6281f81949ec9de0636d98a004b49ba83b0a
0rio1 <oriol.25.ca@gmail.com>
2026-07-17 20:52:24 +0200

    GameJam V.13 (o 14 ns)

--- Assets/Scripts/Minijuego 6/Pomo.cs
+++ Assets/Scripts/Minijuego 6/Pomo.cs
@@ -4,12 +4,16 @@ using UnityEngine.SceneManagement;
 public class Pomo : InteractableObject
 {
    [SerializeField] private string nextScene;
+   [SerializeField] private Sprite sprite;
+   [SerializeField] private SpriteRenderer spriteRennerer;
+
    /// <summary>
     /// Sobrescribe el método de interacción definido en la clase base.
     /// </summary>
     public override void OnInteract()
     {
-        print("TotOK");
+        spriteRennerer.sprite = sprite;
+        transform.GetChild(0).gameObject.SetActive(false);
         // Carga la escena llamada "Escena 2".
         SceneManager.LoadScene(nextScene);
     }
